69a6bf85131594c701a8c0bdb7b6f4047d426242
kirill <semenovkirill.spb@gmail.com>
2016-03-19 20:32:45 +0300

    graph interface

---
+++ src/main/java/org/tirnak/salesman/GraphCalculator.java
@@ -0,0 +1,17 @@
+package org.tirnak.salesman;
+
+import org.tirnak.salesman.model.Edge;
+import org.tirnak.salesman.model.Vertex;
+import org.tirnak.salesman.service.GraphService;
+
+import java.util.List;
+
+/**
+ * Created by kirill on 19.03.16.
+ */
+public class GraphCalculator implements GraphService{
+    @Override
+    public List<Edge> getRoute(Vertex from, Vertex to) {
+        return null;
+    }
+}
